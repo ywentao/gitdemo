9d57f7c256b3ea8d433bbd219570ef83f8b3f08d
ywentao <ywentao@live.cn>
2015-12-14 11:50:38 +0800

    test

---
+++ demo/demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this is git demo test");
+            Console.Read();
+        }
+    }
+}
